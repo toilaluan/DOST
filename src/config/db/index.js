f456eeb445690c9a70931987bf455185facdc939
luantran <toilaluan@github.com>
2023-04-08 20:58:18 +0700

    merge "master" to "luan_develop"

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1/my_database');
+        await mongoose.connect('mongodb://127.0.0.1/doc_stock');
         console.log('Connect to DB successfully');
     } catch (error) {
         console.log(error);
