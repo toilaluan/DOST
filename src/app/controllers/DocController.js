35161332d97d2071f9af88c71b3e140886c06dbf
luantran <toilaluan@github.com>
2023-04-16 15:29:56 +0700

    finalize chat for doc

--- src/app/controllers/DocController.js
+++ src/app/controllers/DocController.js
@@ -11,39 +11,43 @@ class ReadController {
   show(req, res) {
     if (!req.session.loggedin) {
       let query = req.query;
-      Doc.findById(query.id, (err, doc) => {
-        if (err) {
-          console.error(err);
-        }
-        if (readableDoc(doc, req)) {
-          doc = doc.toObject();
-          let id = gdUtils.getFileIdFromUrl(doc.link);
-          let previewLink = gdUtils.idToPreviewLink(id);
-          doc.link = previewLink;
-          res.render("docs/show", doc);
-        } else {
-          res.redirect("/");
-        }
-      });
+      Doc.findById(query.id)
+        .select({ embed: 0 })
+        .exec((err, doc) => {
+          if (err) {
+            console.error(err);
+          }
+          if (readableDoc(doc, req)) {
+            doc = doc.toObject();
+            let id = gdUtils.getFileIdFromUrl(doc.link);
+            let previewLink = gdUtils.idToPreviewLink(id);
+            doc.link = previewLink;
+            res.render("docs/show", doc);
+          } else {
+            res.redirect("/");
+          }
+        });
     } else {
       let query = req.query;
-      Doc.findById(query.id, (err, doc) => {
-        if (err) {
-          console.error(err);
-        }
-        if (readableDoc(doc, req)) {
-          doc = doc.toObject();
-          let id = gdUtils.getFileIdFromUrl(doc.link);
-          let previewLink = gdUtils.idToPreviewLink(id);
-          doc.link = previewLink;
-          res.render(
-            "docs/show",
-            Object.assign({}, doc, { layout: "main_logined" })
-          );
-        } else {
-          res.redirect("/");
-        }
-      });
+      Doc.findById(query.id)
+        .select({ embed: 0 })
+        .exec((err, doc) => {
+          if (err) {
+            console.error(err);
+          }
+          if (readableDoc(doc, req)) {
+            doc = doc.toObject();
+            let id = gdUtils.getFileIdFromUrl(doc.link);
+            let previewLink = gdUtils.idToPreviewLink(id);
+            doc.link = previewLink;
+            res.render(
+              "docs/show",
+              Object.assign({}, doc, { layout: "main_logined" })
+            );
+          } else {
+            res.redirect("/");
+          }
+        });
     }
   }
   upload(req, res, next) {
@@ -51,21 +55,6 @@ class ReadController {
   }
   store(req, res) {
     if (req.file) {
-      // res.render("docs/store", {
-      //   price: 0,
-      //   downloads: 0,
-      //   views: 0,
-      //   created_at: {
-      //     $date: "2023-04-13T16:40:49.000Z",
-      //   },
-      //   title:
-      //     "Grounding DINO: Marrying DINO with Grounded Pre-Training for Open-Set Object Detection\r\n",
-      //   summary:
-      //     "This paper proposes an open-set object detector called Grounding DINO, which marries Transformer-based detector DINO with grounded pre-training to detect arbitrary objects with human inputs such as category names or referring expressions. The proposed model performs remarkably well on benchmarks on COCO, LVIS, ODinW, and RefCOCO/+/g, achieving a 52.5AP on the COCO detection zero-shot transfer benchmark and setting a new record on the ODinW zero-shot benchmark with a mean 26.1AP.\r\n",
-      //   tags: "open-set object detection, Transformer-based detector, grounded pre-training, human inputs, benchmarks, COCO, LVIS, ODinW, RefCOCO/+/g.",
-      //   link: "https://drive.google.com/file/d/1wVoomJ9JPaj8MnXIyWlXGECD1idAVEN1",
-      //   __v: 0,
-      // });
       authorize()
         .then((client) => uploadFile(client, req))
         .then((uploadedFile) => call_api(uploadedFile, req))
@@ -76,15 +65,32 @@ class ReadController {
   async store_confirm(req, res) {
     await Doc.create(req.body, (err, res) => {
       if (err) throw err;
+      const data = {
+        id: res._id,
+      };
+      axios
+        .post("http://localhost:8000/upload_init_embed", data)
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
       console.log("Upload successfully!");
     });
     res.redirect("/");
   }
   chat(req, res) {
     Doc.findById(req.query.id, (err, doc) => {
-      console.log(doc.link);
+      if (err) {
+        console.error(err);
+      }
+      doc = doc.toObject();
+      let id = gdUtils.getFileIdFromUrl(doc.link);
+      let previewLink = gdUtils.idToPreviewLink(id);
+      doc.link = previewLink;
       const data = {
-        link: doc.link,
+        id: req.query.id,
       };
       axios
         .post("http://localhost:8000/init", data)
@@ -94,7 +100,7 @@ class ReadController {
         .catch((error) => {
           console.error(error);
         });
-      res.render("docs/chat", { id: req.query.id });
+      res.render("docs/chat", doc);
     });
   }
 }
